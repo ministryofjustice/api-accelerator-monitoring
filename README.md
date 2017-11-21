eb7a5f7eb535af9fb08492b8f4e66283f750991a
Nick Redshaw <nickredshaw@gmail.com>
2017-11-21 16:03:43 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+Delius API Monitoring
+====
+Monitoring for the Delius API project.
+ 
+The project is based on Smashing.
+
+
+Build
+----
+```
+gem install bundler
+bundle install
+```
+
+Run
+----
+```
+smashing start
+```
+
+Local
+----
+http://localhost:3030/circle
+
+Heroku
+----
+https://delius-api-monitoring.herokuapp.com/circle
+
+
+Further Reading on Smashing
+----
+Check out http://smashing.github.io/ for more information.
---
+++ jobs/health.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require 'httparty'
+require 'ap'
+
+#
+### Global Config
+#
+# httptimeout => Number in seconds for HTTP Timeout. Set to ruby default of 60 seconds.
+# ping_count => Number of pings to perform for the ping method
+#
+HTTP_TIMEOUT = 60
+PING_COUNT = 10
+
+#
+# Check whether a server is Responding you can set a server to
+# check via http request or ping
+#
+# Server Options
+#   name
+#       => The name of the Server Status Tile to Update
+#   url
+#       => Either a website url or an IP address. Do not include https:// when using ping method.
+#   method
+#       => http
+#       => ping
+#
+# Notes:
+#   => If the server you're checking redirects (from http to https for example)
+#      the check will return false
+#
+
+servers = [
+  { name: 'delius-api-dev', url: 'http://deliusapi-dev.sbw4jt6rsq.eu-west-2.elasticbeanstalk.com/api' },
+  { name: 'delius-api-stage', url: 'http://deliusapi-stage.xxxxxx.eu-west-2.elasticbeanstalk.com/api' },
+  { name: 'delius-api-prod', url: 'http://deliusapi-prod.xxxxxx.eu-west-2.elasticbeanstalk.com/api' },
+]
+
+def gather_health_data(server)
+
+  begin
+    response_health = HTTParty.get("#{server[:url]}/health", headers: { 'Accept' => 'application/json' }, timeout: 5)
+    response_info = HTTParty.get("#{server[:url]}/info", headers: { 'Accept' => 'application/json' }, timeout: 5)
+
+    return {
+        status: response_health['status'],
+        ldap: response_health['ldap']['status'],\
+        db: response_health['db']['status'],
+        gitRef: response_info['git']['commit']['id']
+    }
+  rescue HTTParty::Error => expection
+    ap expection.class
+    return { status: 'error', gitRef: expection.class, ldap: "N/A", db: "N/A" }
+  rescue StandardError => expection
+    ap expection.class
+    return { status: 'error', gitRef: expection.class, ldap: "N/A", db: "N/A" }
+  end
+end
+
+SCHEDULER.every '60s', first_in: 0 do |_job|
+  servers.each do |server|
+    result = gather_health_data(server)
+    send_event(server[:name], result: result)
+  end
+end
